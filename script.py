a942ca4dc8f445c3abbed3805e5bc00d58d7d1e2
Bea <beatrice.trinidad@gmail.com>
2020-11-25 11:09:43 +0100

    Trying pandas on MovieLens dataset

--- script.py
+++ script.py
@@ -1,6 +1,26 @@
 from surprise import Dataset
+import pandas as pd
 
 # Load the movielens-100k dataset (download it if needed),
 data = Dataset.load_builtin('ml-100k')
 
-print("Data successfully loaded")
+# pass in column names for each CSV
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
+                    encoding='latin-1')
+
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
+                      encoding='latin-1')
+
+# the movies file contains columns indicating the movie's genres
+# let's only load the first five columns of the file with usecols
+m_cols = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
+movies = pd.read_csv('ml-100k/u.item', sep='|', names=m_cols, usecols=range(5),
+                     encoding='latin-1')
+
+# create one merged DataFrame
+movie_ratings = pd.merge(movies, ratings)
+lens = pd.merge(movie_ratings, users)
+
+print(lens)
