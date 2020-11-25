e69c85084208819eb43b6a4c1192a1ba933595a2
Bea <beatrice.trinidad@gmail.com>
2020-11-25 16:56:00 +0100

    Made movies into columns

--- script.py
+++ script.py
@@ -1,10 +1,10 @@
 from surprise import Dataset
 import pandas as pd
 
-# Load the movielens-100k dataset (download it if needed),
+# Load the movielens-100k dataset
 data = Dataset.load_builtin('ml-100k')
 
-# pass in column names for each CSV
+# Pass in column names for each data set needed
 u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
 users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
                     encoding='latin-1')
@@ -13,14 +13,25 @@ r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
                       encoding='latin-1')
 
-# the movies file contains columns indicating the movie's genres
-# let's only load the first five columns of the file with usecols
+# The movies file contains columns indicating the movie's genres
+# Load only the first five columns of the file with usecols
 m_cols = ['movie_id', 'title', 'release_date', 'video_release_date', 'imdb_url']
 movies = pd.read_csv('ml-100k/u.item', sep='|', names=m_cols, usecols=range(5),
                      encoding='latin-1')
 
-# create one merged DataFrame
+# Create one merged DataFrame with pandas
 movie_ratings = pd.merge(movies, ratings)
 lens = pd.merge(movie_ratings, users)
 
-print(lens)
+df = pd.DataFrame(lens)
+
+# Filter out unnecessary columns
+filterData = df[['user_id','movie_id','title']]
+df = filterData
+
+# Make movies into columns
+movieAsColumns = df.pivot(index='user_id', columns='movie_id', values='title')
+df = movieAsColumns
+
+print (df)
+
