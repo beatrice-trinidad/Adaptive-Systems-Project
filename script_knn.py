54ad0192ebb34f806207ecce976cf7ca588e4feb
Bea <beatrice.trinidad@gmail.com>
2020-12-01 16:29:02 +0100

    Made 25% of cells null

--- script_knn.py
+++ script_knn.py
@@ -1,15 +1,16 @@
 from surprise import Dataset
+from surprise import KNNBasic
 import pandas as pd
+import numpy as np
 
 # Load the full dataset
-movieData = pd.read_csv('movieData.csv', index_col=0, )
+movieData = pd.read_csv('movieData.csv', index_col=0)
 print(movieData.head())
 
-# Randomly select 75% of rows in dataframe
+# Randomly select 75% to be filled in dataframe
 # This is done since the dataset is full, but Step 1 asks for 25% of empty cells.
-movieData_one = movieData.sample(frac=0.75)
-print(movieData_one)
+# The fraction we want to sample is the fraction of non-null values wanted in the resulting frame 
+movieData_25empty = movieData.stack().sample(frac=0.75).unstack()
+print (movieData_25empty)
 
-# Pass in column names for each data set needed
-
-
+# Run "k-nearest-neighbors (k-NN) with users" algorithm
