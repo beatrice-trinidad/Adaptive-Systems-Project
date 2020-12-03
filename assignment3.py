433c23b21f9268f22c8a03af58b2d6b045fad862
Bea <beatrice.trinidad@gmail.com>
2020-12-03 15:59:22 +0100

    Return the top-N recommendations for each user

--- assignment3.py
+++ assignment3.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from surprise import Dataset
 from surprise import KNNBasic
 from surprise import Reader
@@ -113,3 +114,39 @@ min_MAE = min(res_array)
 print(res_array)
 print("Minimum Mean Absolute Error: ", min_MAE)
 print("Best K: ", res_array.index(min_MAE)+1)
+
+def get_top_n(predictions, n=10):
+    """Return the top-N recommendation for each user from a set of predictions.
+
+    Args:
+        predictions(list of Prediction objects): The list of predictions, as
+            returned by the test method of an algorithm.
+        n(int): The number of recommendation to output for each user. Default
+            is 10.
+
+    Returns:
+    A dict where keys are user (raw) ids and values are lists of tuples:
+        [(raw item id, rating estimation), ...] of size n.
+    """
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    return top_n
+
+# Than predict ratings for all pairs (u, i) that are NOT in the training set.
+testset = trainset.build_anti_testset()
+predictions = algo.test(testset)
+
+top_n = get_top_n(predictions, n=10)
+
+# Print the recommended items for each user
+for uid, user_ratings in top_n.items():
+    print(uid, [iid for (iid, _) in user_ratings])
