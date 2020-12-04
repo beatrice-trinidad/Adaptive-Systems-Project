1c306f559883856ec6988aed1bdbff4e41285cee
Giona Fossati <fossatigiona@gmail.com>
2020-12-04 13:54:01 +0100

    Precision & Recall returning 0

--- assignment3.py
+++ assignment3.py
@@ -4,6 +4,7 @@ from surprise import SVD, KNNBasic
 from surprise import Reader
 from surprise.model_selection import train_test_split
 from surprise.model_selection import cross_validate
+from surprise.model_selection import KFold
 import pandas as pd
 import surprise as surprise
 import numpy
@@ -172,6 +173,63 @@ print("***** TOP 2 RECOMMENDED ITEMS FOR EACH USER WITH KNN ******")
 for uid, user_ratings in top_n.items():
     print(uid, [iid for (iid, _) in user_ratings])
 
+
+###                                 ###
+###     PRECISION AND RECALL 25%    ###
+###                                 ###
+
+print("***** PRECISION AND RECALL ******")
+
+def precision_recall_at_k(predictions, k=10, threshold=3.5):
+    """Return precision and recall at k metrics for each user"""
+
+    # First map the predictions to each user.
+    user_est_true = defaultdict(list)
+    for uid, _, true_r, est, _ in predictions:
+        user_est_true[uid].append((est, true_r))
+
+    precisions = dict()
+    recalls = dict()
+    for uid, user_ratings in user_est_true.items():
+
+        # Sort user ratings by estimated value
+        user_ratings.sort(key=lambda x: x[0], reverse=True)
+
+        # Number of relevant items
+        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
+
+        # Number of recommended items in top k
+        n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
+
+        # Number of relevant and recommended items in top k
+        n_rel_and_rec_k = sum(((true_r >= threshold) and (est >= threshold))
+                              for (est, true_r) in user_ratings[:k])
+
+        # Precision@K: Proportion of recommended items that are relevant
+        # When n_rec_k is 0, Precision is undefined. We here set it to 0.
+
+        precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 0
+
+        # Recall@K: Proportion of relevant items that are recommended
+        # When n_rel is 0, Recall is undefined. We here set it to 0.
+
+        recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 0
+
+    return precisions, recalls
+
+## data = Dataset.load_builtin('ml-100k')
+kf = KFold(n_splits=5)
+algo = SVD()
+
+
+for trainset, testset in kf.split(data):
+    algo.fit(trainset)
+    predictions = algo.test(testset)
+    precisions, recalls = precision_recall_at_k(predictions, k=10, threshold=3.5)
+
+    # Precision and recall can then be averaged over all users
+    print(sum(prec for prec in precisions.values()) / len(precisions))
+    print(sum(rec for rec in recalls.values()) / len(recalls))
 
 ######################################################
 #### SPARSITY PROBLEM: TASK #2
